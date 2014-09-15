8dcaf4b6fe8121b3c4f4b5727e4fd94eb40f2915
javierBarajas <barajas_1000@hotmail.com>
2014-09-15 13:18:54 -0500

    word

--- SampleEditorsDevExpress/SampleEditorsDevExpress/frmWord.cs
+++ SampleEditorsDevExpress/SampleEditorsDevExpress/frmWord.cs
@@ -11,9 +11,34 @@ namespace SampleEditorsDevExpress
 {
     public partial class frmWord : Form
     {
+        public class Employee
+        {
+            public Employee()
+            { }
+            private string _Nombre;
+            public string Nombre
+            {
+                get { return _Nombre; }
+                set { _Nombre = value; }
+            }
+            private string _Direccion;
+            public string Direccion
+            {
+                get { return _Direccion; }
+                set { _Direccion = value; }
+            }
+
+        }
         public frmWord()
         {
             InitializeComponent();
+
+            List<Employee> dataSourece = new List<Employee>();
+            dataSourece.Add(new Employee() { Nombre = "John", Direccion = "1st street"});
+            dataSourece.Add(new Employee() { Nombre = "Steve", Direccion = "7th Ave." });
+            
+            richEditControl1.Options.MailMerge.DataSource = dataSourece;
+            dataNavigator1.DataSource = dataSourece; 
         }
     }
 }
